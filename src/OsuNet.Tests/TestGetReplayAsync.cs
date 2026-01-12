1110e86eab510dbd6dba5aa3a2b2007da9860482
WhiteCat0834 <Nullableuserexpection@gmail.com>
2026-01-12 03:11:57 +0300

    test: unused libraries were removed

--- src/OsuNet.Tests/TestGetReplayAsync.cs
+++ src/OsuNet.Tests/TestGetReplayAsync.cs
@@ -1,12 +1,8 @@
 ﻿using NUnit.Framework;
 using OsuNet.Models.Options;
 using RichardSzalay.MockHttp;
-using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace OsuNet.Tests
