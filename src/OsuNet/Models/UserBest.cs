75394cab2f4651881aef42c0381b4949120f3404
WhiteCat0834 <Nullableuserexpection@gmail.com>
2026-05-05 22:58:34 +0300

    fix: remove methods from models

--- src/OsuNet/Models/UserBest.cs
+++ src/OsuNet/Models/UserBest.cs
@@ -107,17 +107,5 @@ namespace OsuNet.Models {
         /// </summary>
         [JsonProperty("replay_available")]
         public bool ReplayAvailable { get; set; }
-
-        /// <summary>
-        /// Gets the avatar URL for this user.
-        /// </summary>
-        /// <returns>A string representing the user's avatar URL.</returns>
-        public string GetAvatar() => $"https://s.ppy.sh/a/{UserId}";
-
-        /// <summary>
-        /// Gets the URL of the user.
-        /// </summary>
-        /// <returns>A string representing the user's URL.</returns>
-        public string GetUrl() => $"https://osu.ppy.sh/users/{UserId}";
     }
 }
