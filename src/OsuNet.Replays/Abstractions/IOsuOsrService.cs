333f7de46145d7570fe3cafc72ede2fe7e625f24
WhiteCat0834 <Nullableuserexpection@gmail.com>
2026-05-30 20:03:49 +0300

    feat: add interface for service

---
+++ src/OsuNet.Replays/Abstractions/IOsuOsrService.cs
@@ -0,0 +1,11 @@
+﻿using OsuNet.Models.Options;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OsuNet.Replays.Abstractions {
+    public interface IOsuOsrService {
+        Task<Stream> GetOsrStreamAsync(GetReplayOptions options, CancellationToken ct = default);
+        Task<byte[]> GetOsrByteAsync(GetReplayOptions options, CancellationToken ct = default);
+    }
+}
