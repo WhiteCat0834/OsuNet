7a1f0d3ec350c9b9ea8a455830c5963e60d2b0c0
WhiteCat0834 <Nullableuserexpection@gmail.com>
2026-05-30 20:14:38 +0300

    fix: service and extension

--- src/OsuNet.Replays/Extensions/OsuApiExtensions.cs
+++ src/OsuNet.Replays/Extensions/OsuApiExtensions.cs
@@ -1,79 +1,26 @@
 ﻿using OsuNet.Abstractions;
 using OsuNet.Models.Options;
-using OsuNet.Replays.Utils;
+using OsuNet.Replays.Abstractions;
+using OsuNet.Replays.Services;
 
 namespace OsuNet.Replays.Extensions {
     /// <summary>
     /// Extension methods for generating .osr replay files from OsuAPI data.
     /// </summary>
     public static class OsuApiExtensions {
+        public static IOsuOsrService CreateOsuOsrService(this IOsuApi api)
+             => new OsuOsrService(api);
+
         /// <summary>
         /// Gets replay as .osr stream.
         /// </summary>
         public static async Task<Stream> GetOsrStreamAsync(this IOsuApi api, GetReplayOptions options, CancellationToken ct = default) 
-            => await api.GetOsrMemoryStreamAsync(options, ct);
+            => await api.CreateOsuOsrService().GetOsrStreamAsync(options, ct);
 
         /// <summary>
         /// Gets replay as .osr byte array.
         /// </summary>
         public static async Task<byte[]> GetOsrByteAsync(this IOsuApi api, GetReplayOptions options, CancellationToken ct = default)
-            => (await api.GetOsrMemoryStreamAsync(options, ct)).ToArray();
-
-        /// <summary>
-        /// Internal method that builds the .osr MemoryStream.
-        /// </summary>
-        internal static async Task<MemoryStream> GetOsrMemoryStreamAsync(this IOsuApi api, GetReplayOptions options, CancellationToken ct = default) {
-            var t1 = api.GetReplayAsync(options, ct);
-            var t2 = api.GetScoresAsync(new GetScoresOptions() {
-                BeatmapId = options.BeatmapId,
-                User = options.User,
-                Mods = options.Mods,
-                Mode = options.Mode,
-                Type = options.Type
-            }, ct);
-            var t3 = api.GetBeatmapsAsync(new GetBeatmapsOptions() {
-                BeatmapId = options.BeatmapId,
-                Type = options.Type,
-                Mode = options.Mode
-            }, ct);
-            await Task.WhenAll(t1, t2, t3);
-
-            var replay = await t1;
-            var score = (await t2).First();
-            var beatmap = (await t3).First();
-
-            var ms = new MemoryStream();
-            var bw = new SerializationWriter(ms);
-
-            var content = Convert.FromBase64String(replay.Content);
-            var replayHashData = CryptoHelper.ComputeMd5Hash(
-                score.MaxCombo + "osu" + score.Username +
-                beatmap.FileMD5 + score.TotalScore + score.Rank
-            );
-
-            bw.Write((byte)options.Mode!);
-            bw.Write(0);                                      // OsuVersion (unable to get value via API)
-            bw.Write(beatmap.FileMD5);
-            bw.Write(score.Username);
-            bw.Write(replayHashData);
-            bw.Write(score.Count300);
-            bw.Write(score.Count100);
-            bw.Write(score.Count50);
-            bw.Write(score.CountGeki);
-            bw.Write(score.CountKatu);
-            bw.Write(score.CountMiss);
-            bw.Write(score.TotalScore);
-            bw.Write(score.MaxCombo);
-            bw.Write((byte)(score.IsPerfect ? 1 : 0));
-            bw.Write((int)score.EnabledMods);
-            bw.Write("");                                     // lifebar (unable to get value via API)
-            bw.Write(score.DateTime);
-            bw.Write(content.Length);
-            bw.Write(content);
-            bw.Write(score.ScoreId);
-
-            ms.Position = 0;
-            return ms;
-        }
+            => (await api.CreateOsuOsrService().GetOsrByteAsync(options, ct));
     }
 }
--- src/OsuNet.Replays/Services/OsuOsrService.cs
+++ src/OsuNet.Replays/Services/OsuOsrService.cs
@@ -1,7 +1,7 @@
 ﻿using OsuNet.Abstractions;
 using OsuNet.Models.Options;
 using OsuNet.Replays.Abstractions;
-using OsuNet.Replays.Extensions;
+using OsuNet.Replays.Utils;
 
 namespace OsuNet.Replays.Services {
     public class OsuOsrService : IOsuOsrService {
@@ -14,12 +14,69 @@ namespace OsuNet.Replays.Services {
         /// Gets replay as .osr stream.
         /// </summary>
         public async Task<Stream> GetOsrStreamAsync(GetReplayOptions options, CancellationToken ct = default)
-            => await api.GetOsrMemoryStreamAsync(options, ct);
+            => await GetOsrMemoryStreamAsync(options, ct);
 
         /// <summary>
         /// Gets replay as .osr byte array.
         /// </summary>
         public async Task<byte[]> GetOsrByteAsync(GetReplayOptions options, CancellationToken ct = default)
-            => (await api.GetOsrMemoryStreamAsync(options, ct)).ToArray();
+            => (await GetOsrMemoryStreamAsync(options, ct)).ToArray();
+
+        /// <summary>
+        /// Internal method that builds the .osr MemoryStream.
+        /// </summary>
+        internal async Task<MemoryStream> GetOsrMemoryStreamAsync(GetReplayOptions options, CancellationToken ct = default) {
+            var t1 = api.GetReplayAsync(options, ct);
+            var t2 = api.GetScoresAsync(new GetScoresOptions() {
+                BeatmapId = options.BeatmapId,
+                User = options.User,
+                Mods = options.Mods,
+                Mode = options.Mode,
+                Type = options.Type
+            }, ct);
+            var t3 = api.GetBeatmapsAsync(new GetBeatmapsOptions() {
+                BeatmapId = options.BeatmapId,
+                Type = options.Type,
+                Mode = options.Mode
+            }, ct);
+            await Task.WhenAll(t1, t2, t3);
+
+            var replay = await t1;
+            var score = (await t2).First();
+            var beatmap = (await t3).First();
+
+            var ms = new MemoryStream();
+            var bw = new SerializationWriter(ms);
+
+            var content = Convert.FromBase64String(replay.Content);
+            var replayHashData = CryptoHelper.ComputeMd5Hash(
+                score.MaxCombo + "osu" + score.Username +
+                beatmap.FileMD5 + score.TotalScore + score.Rank
+            );
+
+            bw.Write((byte)options.Mode!);
+            bw.Write(0);                                      // OsuVersion (unable to get value via API)
+            bw.Write(beatmap.FileMD5);
+            bw.Write(score.Username);
+            bw.Write(replayHashData);
+            bw.Write(score.Count300);
+            bw.Write(score.Count100);
+            bw.Write(score.Count50);
+            bw.Write(score.CountGeki);
+            bw.Write(score.CountKatu);
+            bw.Write(score.CountMiss);
+            bw.Write(score.TotalScore);
+            bw.Write(score.MaxCombo);
+            bw.Write((byte)(score.IsPerfect ? 1 : 0));
+            bw.Write((int)score.EnabledMods);
+            bw.Write("");                                     // lifebar (unable to get value via API)
+            bw.Write(score.DateTime);
+            bw.Write(content.Length);
+            bw.Write(content);
+            bw.Write(score.ScoreId);
+
+            ms.Position = 0;
+            return ms;
+        }
     }
 }
