b428a098909cf0fda6861e56b9469d3c15f3075c
WhiteCat0834 <Nullableuserexpection@gmail.com>
2026-05-30 20:04:03 +0300

    feat: add osuosrservice

---
+++ src/OsuNet.Replays/Services/OsuOsrService.cs
@@ -0,0 +1,25 @@
+﻿using OsuNet.Abstractions;
+using OsuNet.Models.Options;
+using OsuNet.Replays.Abstractions;
+using OsuNet.Replays.Extensions;
+
+namespace OsuNet.Replays.Services {
+    public class OsuOsrService : IOsuOsrService {
+        private readonly IOsuApi api;
+
+        public OsuOsrService(IOsuApi api) { 
+            this.api = api; 
+        }
+        /// <summary>
+        /// Gets replay as .osr stream.
+        /// </summary>
+        public async Task<Stream> GetOsrStreamAsync(GetReplayOptions options, CancellationToken ct = default)
+            => await api.GetOsrMemoryStreamAsync(options, ct);
+
+        /// <summary>
+        /// Gets replay as .osr byte array.
+        /// </summary>
+        public async Task<byte[]> GetOsrByteAsync(GetReplayOptions options, CancellationToken ct = default)
+            => (await api.GetOsrMemoryStreamAsync(options, ct)).ToArray();
+    }
+}
