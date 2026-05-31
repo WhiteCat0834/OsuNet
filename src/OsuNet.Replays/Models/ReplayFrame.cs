18d921ddd7a15eaf73ce70dba1eca642716bb023
WhiteCat0834 <Nullableuserexpection@gmail.com>
2026-05-31 04:05:34 +0300

    feat: add replayframe

---
+++ src/OsuNet.Replays/Models/ReplayFrame.cs
@@ -0,0 +1,9 @@
+﻿namespace OsuNet.Replays.Models {
+    public class ReplayFrame {
+        public int TimeDiff { get; set; }
+        public int Time { get; set; }
+        public float X { get; set; }
+        public float Y { get; set; }
+        public int RawKeys { get; set; }
+    }
+}
