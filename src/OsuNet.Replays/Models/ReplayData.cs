8d84350bc994863b829cdf6ccc7816d0b96571b7
WhiteCat0834 <Nullableuserexpection@gmail.com>
2026-05-31 20:30:46 +0300

    fix: remove unused deps

--- src/OsuNet.Replays/Models/ReplayData.cs
+++ src/OsuNet.Replays/Models/ReplayData.cs
@@ -1,6 +1,4 @@
-﻿using OsuNet.Replays.Abstractions;
-
-namespace OsuNet.Replays.Models {
+﻿namespace OsuNet.Replays.Models {
     public class ReplayData {
         public int Seed { get; set; }
         public List<ReplayFrame> ReplayFrames { get; set; } = new List<ReplayFrame>();
