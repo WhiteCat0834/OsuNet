5bf21dd7c98a8499399701151ffe73e9604f711f
WhiteCat0834 <Nullableuserexpection@gmail.com>
2026-05-31 20:30:30 +0300

    feat: add replayframes

---
+++ src/OsuNet.Replays/Models/ManiaReplayFrame.cs
@@ -0,0 +1,7 @@
+﻿using OsuNet.Replays.Enums;
+
+namespace OsuNet.Replays.Models {
+    public class ManiaReplayFrame : ReplayFrame {
+        public ManiaKeys ManiaKeys => (ManiaKeys)RawKeys;
+    }
+}
---
+++ src/OsuNet.Replays/Models/OsuReplayFrame.cs
@@ -0,0 +1,7 @@
+﻿using OsuNet.Replays.Enums;
+
+namespace OsuNet.Replays.Models {
+    public class OsuReplayFrame : ReplayFrame {
+        public OsuKeys OsuKeys => (OsuKeys)RawKeys;
+    }
+}
