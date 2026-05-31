f472563b231c4001bafa29a4842742c447c13e2f
WhiteCat0834 <Nullableuserexpection@gmail.com>
2026-05-31 04:05:42 +0300

    feat: add replaydata

---
+++ src/OsuNet.Replays/Models/ReplayData.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OsuNet.Replays.Models {
+    public class ReplayData {
+        public int Seed { get; set; }
+        public List<ReplayFrame> ReplayFrames { get; set; } = new List<ReplayFrame>();
+    }
+}
