54f9888d1fc295f685f204e018fde7cd06ed6720
WhiteCat0834 <Nullableuserexpection@gmail.com>
2026-05-31 20:29:53 +0300

    feat: add keys
    CatchKeys need manual test

---
+++ src/OsuNet.Replays/Enums/CatchKeys.cs
@@ -0,0 +1,9 @@
+﻿namespace OsuNet.Replays.Enums {
+    [Flags]
+    public enum CatchKeys {
+        None = 0,
+        MoveLeft = 1 << 0,
+        MoveRight = 1 << 1,
+        Dash = 1 << 2
+    }
+}
---
+++ src/OsuNet.Replays/Enums/TaikoKeys.cs
@@ -0,0 +1,10 @@
+﻿namespace OsuNet.Replays.Enums {
+    [Flags]
+    public enum TaikoKeys {
+        None = 0,
+        LeftRed = 1 << 0,
+        LeftBlue = 1 << 1,
+        RightRed = 1 << 2,
+        RightBlue = 1 << 3
+    }
+}
