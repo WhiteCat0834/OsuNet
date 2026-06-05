513ee08e42c55de09b8dedf4120ee717269cb924
WhiteCat0834 <Nullableuserexpection@gmail.com>
2026-06-06 00:18:42 +0300

    test: add tests for enums

---
+++ test/OsuNet.Replays.Tests/Enums/CatchKeysTests.cs
@@ -0,0 +1,12 @@
+﻿using OsuNet.Replays.Enums;
+
+namespace OsuNet.Replays.Tests.Enums {
+    public class CatchKeysTests {
+        [Fact]
+        public void CatchKeysEnum_ShouldContainExpectedValues() {
+            // Assert
+            Assert.Equal(0, (int)CatchKeys.None);
+            Assert.Equal(1, (int)CatchKeys.Dash);
+        }
+    }
+}
