ec4dd1147e35c81c9fcb90c4c942d77282e0a1cd
WhiteCat0834 <Nullableuserexpection@gmail.com>
2026-09-05 01:58:30 +0300

    fix: add osuapirequester

--- src/OsuNet/OsuApi.cs
+++ src/OsuNet/OsuApi.cs
@@ -1,23 +1,20 @@
-﻿using Newtonsoft.Json;
-using OsuNet.Abstractions;
-using OsuNet.Converters;
+﻿using OsuNet.Abstractions;
 using OsuNet.Modules;
-using System.Net;
 
 namespace OsuNet {
     /// <summary>
     /// The main class of this library.
     /// </summary>
-    public partial class OsuApi : IOsuApi {
+    public partial class OsuApi : IOsuApi {        
+        private readonly IApiRequester apiRequester;
+
         /// <summary>
-        /// Osu!API token.
+        /// Gets or sets the Osu!API token. 
         /// </summary>
-        public string AccessToken { get; set; }
-        private readonly HttpClient httpClient;
-        private const string baseUrl = "https://osu.ppy.sh/api/";
-        private static readonly JsonSerializerSettings jsonSettings = new() {
-            Converters = { new OsuBoolConverter() }
-        };
+        public string AccessToken {
+            get => apiRequester.AccessToken;
+            set => apiRequester.AccessToken = value;
+        }
 
         /// <summary>
         /// Represents a module for interacting with beatmap-related endpoints in the osu! API.
@@ -45,73 +42,31 @@ namespace OsuNet {
         public IReplayModule Replay { get; }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="OsuApi"/> class.
+        /// Initializes a new instance of the <see cref="OsuApi"/> class with a default HTTP requester.
         /// </summary>
         /// <param name="accessToken">Your Osu!API token.</param>
         public OsuApi(string accessToken) {
-            if (string.IsNullOrWhiteSpace(accessToken))
-                throw new ArgumentNullException(nameof(accessToken), "Access token cannot be null or empty.");
-
-            this.AccessToken = accessToken;
-            httpClient = new HttpClient(new HttpClientHandler { 
-                AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate
-            });
-            httpClient.DefaultRequestHeaders.Add("Accept-Encoding", "gzip, deflate");
-
-            this.Beatmaps = new BeatmapsModule(this);
-            this.User = new UserModule(this);
-            this.Scores = new ScoresModule(this);
-            this.Multiplayer = new MultiplayerModule(this);
-            this.Replay = new ReplayModule(this);
-        }
-
-        private T fromJson<T>(Stream stream) {
-            using var reader = new StreamReader(stream);
-            using var jsonReader = new JsonTextReader(reader);
-            var serializer = JsonSerializer.Create(jsonSettings);
-            return serializer.Deserialize<T>(jsonReader)!;
+            this.apiRequester = new OsuApiRequester(accessToken);
+            
+            this.Beatmaps = new BeatmapsModule(this.apiRequester);
+            this.User = new UserModule(this.apiRequester);
+            this.Scores = new ScoresModule(this.apiRequester);
+            this.Multiplayer = new MultiplayerModule(this.apiRequester);
+            this.Replay = new ReplayModule(this.apiRequester);
         }
 
         /// <summary>
-        /// Asynchronously executes an HTTP GET request to the specified API endpoint with the provided query parameters
-        /// and returns the deserialized JSON response. Query parameter values are automatically URL-encoded to ensure
-        /// safe transmission over HTTP. The method will throw an exception if the server returns a non-successful status code.
+        /// Initializes a new instance of the <see cref="OsuApi"/> class with a custom requester.
         /// </summary>
-        /// <typeparam name="T">The expected type of the deserialized JSON response object.</typeparam>
-        /// <param name="endpoint">The relative API endpoint path (e.g., "get_beatmaps", "get_user") to append to the base URL.</param>
-        /// <param name="query">
-        /// A collection of key-value pairs representing the query string parameters to include in the request.
-        /// Both keys and values will be URL-encoded using <see cref="Uri.EscapeDataString"/>.
-        /// </param>
-        /// <param name="cancellationToken">
-        /// A cancellation token that can be used to cancel the asynchronous operation, including the HTTP request
-        /// and the subsequent JSON deserialization.
-        /// </param>
-        /// <returns>
-        /// A task representing the asynchronous operation, containing the deserialized response object of type <typeparamref name="T"/>.
-        /// </returns>
-        /// <exception cref="HttpRequestException">
-        /// Thrown when the HTTP request completes with a non-successful status code (4xx or 5xx).
-        /// Propagated by <see cref="HttpResponseMessage.EnsureSuccessStatusCode"/>.
-        /// </exception>
-        /// <exception cref="OperationCanceledException">
-        /// Thrown when the <paramref name="cancellationToken"/> is signaled before the operation completes.
-        /// </exception>
-        /// <exception cref="TaskCanceledException">
-        /// Thrown when the underlying HTTP request times out or is canceled.
-        /// </exception>
-        /// <remarks>
-        /// The method constructs the full request URL by combining the configured base URL, the endpoint path,
-        /// and a URL-encoded query string. The response content is read as a stream and deserialized using the
-        /// configured JSON deserializer.
-        /// </remarks>
-        public async Task<T> GetAsync<T>(string endpoint, IEnumerable<KeyValuePair<string, string>> query, CancellationToken cancellationToken = default) {
-            var queryString = string.Join("&", query.Select(kv => $"{kv.Key}={Uri.EscapeDataString(kv.Value)}"));
-            var url = $"{baseUrl}{endpoint}?{queryString}";
-            using var response = await httpClient.GetAsync(url, cancellationToken);
-            response.EnsureSuccessStatusCode();
-            await using var stream = await response.Content.ReadAsStreamAsync(cancellationToken);
-            return fromJson<T>(stream);
+        /// <param name="requester">A custom implementation of <see cref="IApiRequester"/>.</param>
+        public OsuApi(IApiRequester requester) {
+            this.apiRequester = requester ?? throw new ArgumentNullException(nameof(requester));
+            
+            this.Beatmaps = new BeatmapsModule(this.apiRequester);
+            this.User = new UserModule(this.apiRequester);
+            this.Scores = new ScoresModule(this.apiRequester);
+            this.Multiplayer = new MultiplayerModule(this.apiRequester);
+            this.Replay = new ReplayModule(this.apiRequester);
         }
     }
 }
