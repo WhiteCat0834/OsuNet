95b4c6b850044d28fc98d78a6c271aff24d6269d
WhiteCat0834 <Nullableuserexpection@gmail.com>
2026-09-21 18:56:47 +0300

    test(fix): fix matchinfotests

--- test/OsuNet.Tests/Models/Info/MatchInfoTests.cs
+++ test/OsuNet.Tests/Models/Info/MatchInfoTests.cs
@@ -1,33 +1,43 @@
-using Newtonsoft.Json;
+using System.Text.Json;
+using OsuNet.Converters;
 using OsuNet.Models.Info;
 
 namespace OsuNet.Tests.Models.Info {
     public class MatchInfoTests {
+
+        private JsonSerializerOptions GetOptions() {
+            return new JsonSerializerOptions {
+                Converters = { new MatchInfoConverter() }
+            };
+        }
+
         [Fact]
-        public void MatchInfo_ShouldBeNullIfZero() {
-            // Arrange & Act
-            var matchInfo = (MatchInfo)0;
+        public void MatchInfo_ShouldBeNullIfApiReturnsZero() {
+            // Arrange
+            var options = GetOptions();
+            var json = "0";
+
+            // Act
+            var matchInfo = JsonSerializer.Deserialize<MatchInfo>(json, options);
 
             // Assert
-            Assert.Equal((ulong)0, matchInfo.MatchId);
-            Assert.Null(matchInfo.Name);
-            Assert.Equal(new DateTime(0001, 01, 01), matchInfo.StartTime);
-            Assert.Null(matchInfo.EndTime);
+            Assert.Null(matchInfo);
         }
 
         [Fact]
         public void MatchInfo_ShouldSerializeAndDeserializeCorrectly() {
             // Arrange
+            var options = GetOptions();
             var matchInfo = new MatchInfo(
                 MatchId: 789,
                 Name: "Test Match",
-                StartTime: new DateTime(2023, 1, 1),
-                EndTime: new DateTime(2023, 1, 2)
+                StartTime: new DateTime(2023, 1, 1, 0, 0, 0, DateTimeKind.Utc),
+                EndTime: new DateTime(2023, 1, 2, 0, 0, 0, DateTimeKind.Utc)
             );
 
             // Act
-            var json = JsonConvert.SerializeObject(matchInfo);
-            var deserializedMatchInfo = JsonConvert.DeserializeObject<MatchInfo>(json);
+            var json = JsonSerializer.Serialize(matchInfo, options);
+            var deserializedMatchInfo = JsonSerializer.Deserialize<MatchInfo>(json, options);
 
             // Assert
             Assert.NotNull(deserializedMatchInfo);
