f8f2e759fa4fa9ece80077eeb1e73867d063dbe2
WhiteCat0834 <Nullableuserexpection@gmail.com>
2026-09-21 17:44:54 +0300

    test(fix): newtonsoft.json to system.text.json

--- test/OsuNet.Tests/Converters/OsuBoolConverterTests.cs
+++ test/OsuNet.Tests/Converters/OsuBoolConverterTests.cs
@@ -1,20 +1,24 @@
-using Newtonsoft.Json;
+using System.Text.Json;
 using OsuNet.Converters;
 
 namespace OsuNet.Tests.Converters {
     public class OsuBoolConverterTests {
+
+        private JsonSerializerOptions GetOptions() {
+            return new JsonSerializerOptions {
+                Converters = { new OsuBoolConverter() }
+            };
+        }
+
         [Theory]
         [InlineData(true, "\"1\"")]
         [InlineData(false, "\"0\"")]
         public void OsuBoolConverter_ShouldSerializeCorrectly(bool value, string expectedJson) {
             // Arrange
-            var converter = new OsuBoolConverter();
-            var settings = new JsonSerializerSettings {
-                Converters = new List<JsonConverter> { converter }
-            };
+            var options = GetOptions();
 
             // Act
-            var json = JsonConvert.SerializeObject(value, settings);
+            var json = JsonSerializer.Serialize(value, options);
 
             // Assert
             Assert.Equal(expectedJson, json);
@@ -31,13 +35,10 @@ namespace OsuNet.Tests.Converters {
         [InlineData("null", false)]
         public void OsuBoolConverter_ShouldDeserializeCorrectly(string json, bool expectedValue) {
             // Arrange
-            var converter = new OsuBoolConverter();
-            var settings = new JsonSerializerSettings {
-                Converters = new List<JsonConverter> { converter }
-            };
+            var options = GetOptions();
 
             // Act
-            var result = JsonConvert.DeserializeObject<bool>(json, settings);
+            var result = JsonSerializer.Deserialize<bool>(json, options);
 
             // Assert
             Assert.Equal(expectedValue, result);
